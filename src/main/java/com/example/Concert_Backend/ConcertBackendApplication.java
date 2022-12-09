82414d3de02a541ca97308ca04f65f666afc24b0
Caitlin Millar <caitlin-millar@outlook.com>
2022-12-09 16:26:07 +0000

    first commit

---
+++ src/main/java/com/example/Concert_Backend/ConcertBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Concert_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertBackendApplication.class, args);
+	}
+
+}
