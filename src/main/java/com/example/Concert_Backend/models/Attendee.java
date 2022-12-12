20d9d917ffec643ea34016fec10f4291e390d1cd
Berna Yasar <bernayasar@hotmail.co.uk>
2022-12-12 10:15:33 +0000

    finished attendee model class

--- src/main/java/com/example/Concert_Backend/models/Attendee.java
+++ src/main/java/com/example/Concert_Backend/models/Attendee.java
@@ -1,6 +1,43 @@
 package com.example.Concert_Backend.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity (name = "attendees")
 public class Attendee {
 
-    // writing something to test
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column (name = "name")
+    private String name;
+
+    @Column (name = "emailAddress")
+    private String emailAddress;
+
+    @Column (name = "phoneNumber")
+    private Long phoneNumber;
+
+    @JsonIgnoreProperties ({"attendees"})
+    private List<Concert> concerts;
+
+    public Attendee(String name, String emailAddress, Long phoneNumber){
+        this.name = name;
+        this.emailAddress = emailAddress;
+        this.phoneNumber = phoneNumber;
+        this.concerts = new ArrayList<>();
+    }
+
+    public Attendee(){}
+
+
+
 }
