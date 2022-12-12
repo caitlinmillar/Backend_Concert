e69e721611a1bf2c0ed441dc9021c4b758d85c34
Loshanth Selvanayagam <loshanth75@googlemail.com>
2022-12-12 12:51:33 +0000

    added methods in attendee service

--- src/main/java/com/example/Concert_Backend/services/AttendeeService.java
+++ src/main/java/com/example/Concert_Backend/services/AttendeeService.java
@@ -1,13 +1,26 @@
 package com.example.Concert_Backend.services;
 
+import com.example.Concert_Backend.models.Attendee;
 import com.example.Concert_Backend.repositories.AttendeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AttendeeService {
     @Autowired
     AttendeeRepository attendeeRepository;
 
+    public List<Attendee>getAllAttendees(){
+        return attendeeRepository.findAll();
+    }
+    public Attendee getAttendeeById(Long id){
+        return attendeeRepository.findById(id).get();
+    }
+    public Attendee addNewAttendee(Attendee attendee){
+        attendeeRepository.save(attendee);
+        return attendee;
+    }
 
 }
